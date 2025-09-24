ef3ad20b4476ddc14d4fd5b8a357d5bb5afe870f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-24 15:43:00 -0300

    REFACTOR: removes class LoginDTO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -13,9 +15,3 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 
 
 app.Run();
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
-}
