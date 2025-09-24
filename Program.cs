8b2270936b4e91624dbc714376bfffba1ffcb6f1
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-24 14:39:59 -0300

    FEAT: adds login endpoint and its class

--- Program.cs
+++ Program.cs
@@ -3,4 +3,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
+        return Results.Ok("Logged in sucessfully");
+    else
+        return Results.Unauthorized();
+});
+
+
 app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
