e5b8702c8d81d8de96bb56648945bb70b6cf5399
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-25 16:14:58 -0300

    FEAT: adds IAdministratorService interface

---
+++ Domain/Interfaces/IAdministratorService.cs
@@ -0,0 +1,9 @@
+using MinimalApi.Domain.Entities;
+using MinimalApi.DTOs;
+
+namespace MinimalApi.Domain.Interfaces;
+
+public interface IAdministratorService
+{
+    List<Administrator> Login(LoginDTO loginDTO);
+}
