9e38482cd30dec11cbf84902a6235d6f13bc0e2d
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-25 15:07:44 -0300

    FEAT: adds MySQL connection string

--- Infrastructure/Database/DataBaseContext.cs
+++ Infrastructure/Database/DataBaseContext.cs
@@ -5,13 +5,24 @@ namespace MinimalApi.Infrastructure.Db;
 
 public class DataBaseContext : DbContext
 {
+    private readonly IConfiguration _configurationAppSettings;
+    public DataBaseContext(IConfiguration configurationAppSettings)
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
     public DbSet<Administrator> Administrators { get; set; } = default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySql(
-            "connection string",
-            ServerVersion.AutoDetect("connection string")
-        );
+        var connectionString = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+        if (!string.IsNullOrEmpty(connectionString))
+        {
+            optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString)
+            );
+        }
+
+
     }
 }
