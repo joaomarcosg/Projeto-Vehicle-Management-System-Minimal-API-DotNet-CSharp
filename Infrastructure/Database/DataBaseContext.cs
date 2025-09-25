81daf02e86b9909512e9b60590cbf41f3ec13e3f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-25 14:44:57 -0300

    FEAT: adds initial settings to create table

--- Infrastructure/Database/DataBaseContext.cs
+++ Infrastructure/Database/DataBaseContext.cs
@@ -1,8 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.Entities;
 
 namespace MinimalApi.Infrastructure.Db;
 
-public class DataBaseContext
+public class DataBaseContext : DbContext
 {
+    public DbSet<Administrator> Administrators { get; set; } = default!;
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "connection string",
+            ServerVersion.AutoDetect("connection string")
+        );
+    }
 }
