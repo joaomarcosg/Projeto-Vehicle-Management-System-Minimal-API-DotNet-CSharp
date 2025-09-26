9a5266c955ca058981d202b5d9ce6be16d515fc6
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-26 15:10:01 -0300

    FEAT: implements Vehicle interface methods

---
+++ Domain/Services/VehicleService.cs
@@ -0,0 +1,35 @@
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
+using MinimalApi.Infrastructure.Db;
+
+namespace MinimalApi.Domain.Services;
+
+public class VehicleService : IVehicleService
+{
+    private readonly DataBaseContext _context;
+    public Vehicle AddVehicle(Vehicle vehicle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteVehicle(Vehicle vehicle)
+    {
+        _context.Vehicles.Remove(vehicle);
+        _context.SaveChanges();
+    }
+
+    public List<Vehicle> ListVehicles(int page = 1, string? name = null, string? mark = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Vehicle SeachById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Vehicle UpdateVehicle(Vehicle vehicle)
+    {
+        throw new NotImplementedException();
+    }
+}
