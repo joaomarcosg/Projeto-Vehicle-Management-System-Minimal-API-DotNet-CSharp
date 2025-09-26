df0981eb6ce795527d4ff2e01eeb2036344a9942
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-26 16:09:00 -0300

    FEAT: adds Home route

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Domain.Interfaces;
+using MinimalApi.Domain.ModelViews;
 using MinimalApi.Domain.Services;
 using MinimalApi.DTOs;
 using MinimalApi.Infrastructure.Db;
@@ -21,7 +22,7 @@ builder.Services.AddDbContext<DataBaseContext>(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) =>
 {
