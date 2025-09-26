32ed9b83253c20e0cb7c0bb1d7ebc3f1a92d1f50
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-26 16:13:34 -0300

    FEAT: adds a welcome message to Home view model

--- Domain/ModelViews/Home.cs
+++ Domain/ModelViews/Home.cs
@@ -2,5 +2,6 @@ namespace MinimalApi.Domain.ModelViews;
 
 public struct Home
 {
+    public string Message { get => "Welcome to Vehicles API - Minimal API"; }
     public string Documentation { get => "/swagger"; }
 }
