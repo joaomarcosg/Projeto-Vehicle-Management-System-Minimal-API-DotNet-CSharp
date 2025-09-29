940436301b2c974963070b824eda22ad5f043d9b
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-29 12:34:54 -0300

    FIX: fixes method syntax

--- Domain/Interfaces/IVehicleService.cs
+++ Domain/Interfaces/IVehicleService.cs
@@ -5,7 +5,7 @@ namespace MinimalApi.Domain.Interfaces;
 public interface IVehicleService
 {
     List<Vehicle> ListVehicles(int? page = 1, string? name = null, string? mark = null);
-    Vehicle? SeachById(int id);
+    Vehicle? SearchById(int id);
     void AddVehicle(Vehicle vehicle);
     void UpdateVehicle(Vehicle vehicle);
     void DeleteVehicle(Vehicle vehicle);
