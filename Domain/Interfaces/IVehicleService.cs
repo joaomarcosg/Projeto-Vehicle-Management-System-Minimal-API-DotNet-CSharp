7dc04649523103b0827108761f507011eb371e1f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-29 12:10:11 -0300

    FIX: fixes page parameter of ListVehicles method

--- Domain/Interfaces/IVehicleService.cs
+++ Domain/Interfaces/IVehicleService.cs
@@ -4,7 +4,7 @@ namespace MinimalApi.Domain.Interfaces;
 
 public interface IVehicleService
 {
-    List<Vehicle> ListVehicles(int page = 1, string? name = null, string? mark = null);
+    List<Vehicle> ListVehicles(int? page = 1, string? name = null, string? mark = null);
     Vehicle? SeachById(int id);
     void AddVehicle(Vehicle vehicle);
     void UpdateVehicle(Vehicle vehicle);
