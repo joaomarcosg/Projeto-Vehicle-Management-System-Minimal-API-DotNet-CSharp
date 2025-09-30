44f22341a2c29a77e62b50aacd6123f7dbf32d84
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-30 15:43:10 -0300

    FIX: fixes syntax

--- Domain/DTOs/AdministratorDTO.cs
+++ Domain/DTOs/AdministratorDTO.cs
@@ -6,5 +6,5 @@ public class AdministratorDTO
 {
     public string Email { get; set; } = default!;
     public string Password { get; set; } = default!;
-    public Profile Profile { get; set; } = default!;
+    public Profile? Profile { get; set; } = default!;
 }
