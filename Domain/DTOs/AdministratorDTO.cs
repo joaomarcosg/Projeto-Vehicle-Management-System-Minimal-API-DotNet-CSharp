e1f07b219dfb3655d84897573b30f5c27815e37e
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-30 15:03:18 -0300

    FEAT: adds AdministratorDTO

---
+++ Domain/DTOs/AdministratorDTO.cs
@@ -0,0 +1,8 @@
+namespace MinimalApi.DTOs;
+
+public class AdministratorDTO
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string Profile { get; set; } = default!;
+}
