172b84ae41bee0e7f6495ba97f031f8a8f2abed7
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-01 15:05:31 -0300

    FEAT: adds Administrator model view

---
+++ Domain/ModelViews/AdministratorModelView.cs
@@ -0,0 +1,10 @@
+using MinimalApi.Domain.Enums;
+
+namespace MinimalApi.Domain.ModelViews;
+
+public record AdministratorModelView
+{
+    public int Id { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public Profile Profile { get; set; } = default!;
+}
