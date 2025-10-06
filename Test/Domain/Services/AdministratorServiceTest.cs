6af83a67ed3005d78c45101a5849b7ee51185e18
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-06 10:17:51 -0300

    TEST: finishes AdministratorService test

--- Test/Domain/Services/AdministratorServiceTest.cs
+++ Test/Domain/Services/AdministratorServiceTest.cs
@@ -1,20 +1,63 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Services;
+using MinimalApi.Infrastructure.Db;
 
 namespace Test.Domain.Services;
 
 [TestClass]
-public class AdministratorTest
+public class AdministratorServiceTest
 {
+    private DbContext CreateTestContext()
+    {
+        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+        return new DataBaseContext(configuration);
+    }
     [TestMethod]
     public void TestSaveAdministrator()
     {
-        var adm = new Administrator();
+        var context = CreateTestContext();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE administrators");
 
-        adm.Id = 1;
+        var adm = new Administrator();
         adm.Email = "teste@teste.com";
         adm.Password = "teste";
         adm.Profile = "Adm";
+        var administratorService = new AdministratorService((DataBaseContext)context);
 
+        administratorService.Add(adm);
+
+        Assert.AreEqual(1, administratorService.ListAdministrators(1).Count());
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("teste", adm.Password);
+        Assert.AreEqual("Adm", adm.Profile);
+
+
+    }
+
+        public void TestSearchAdministratorById()
+    {   
+        var context = CreateTestContext();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE administrators");
+
+        var adm = new Administrator();
+        adm.Email = "teste@teste.com";
+        adm.Password = "teste";
+        adm.Profile = "Adm";
+        var administratorService = new AdministratorService((DataBaseContext)context);
+
+        administratorService.Add(adm);
+        var admDatabase = administratorService.SearchById(adm.Id);
+
+        Assert.AreEqual(1, admDatabase.Id);
         
     }
 }
