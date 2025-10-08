d936a3e33fab3bb1364f85dc9edb0550260f0d72
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-08 15:53:22 -0300

    TEST: adds settings for test

--- Test/Requests/AdministratorRequestTest.cs
+++ Test/Requests/AdministratorRequestTest.cs
@@ -1,11 +1,35 @@
+using System.Text;
+using System.Text.Json;
+using MinimalApi.DTOs;
+using Test.Helpers;
+
 namespace Test.Requests;
 
 [TestClass]
 public class AdministratorRequestTest
 {
-    [TestMethod]
-    public void TestGetAndSetProperties()
+    [ClassInitialize]
+    public static void ClassInt(TestContext testContext)
     {
-        
+        Setup.ClassInit(testContext);
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Setup.ClassCleanup();
+    }
+
+    [TestMethod]
+    public async Task TestGetAndSetProperties()
+    {
+        var loginDTO = new LoginDTO
+        {
+            Email = "adm@teste.com",
+            Password = "123456"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "application/json");
+
     }
 }
